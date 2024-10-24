f4c35879faba55dd1f36fb49e01f40ea6a2faaec
puffynNeroun <130913559+puffynNeroun@users.noreply.github.com>
2024-10-24 20:17:17 +0300

    add animation about

--- src/js/app.js
+++ src/js/app.js
@@ -30,4 +30,38 @@ document.addEventListener("DOMContentLoaded", () => {
         delay: 0.6,
         ease: "power2.out"
     });
+
+    gsap.from(".about-site__title, .about-site__subtitle", {
+        y: 20,
+        opacity: 0,
+        duration: 1.2,
+        ease: "power3.out",
+        stagger: 0.3,
+    });
+
+    gsap.from(".about-site__card", {
+        opacity: 0,
+        y: 50,
+        duration: 1.5,
+        ease: "power3.out",
+        stagger: 0.3,
+        delay: 0.5,
+    });
+
+    gsap.from(".about-site__img img", {
+        scale: 0.8,
+        opacity: 0,
+        duration: 1.5,
+        ease: "power2.out",
+        delay: 0.3,
+    });
+
+    gsap.to(".about-site__img img", {
+        rotation: 3,
+        yoyo: true,
+        repeat: -1,
+        duration: 1,
+        ease: "sine.inOut",
+        yoyoEase: true,
+    });
 });
