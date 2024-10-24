c4f7d124b8bdc07ab5e297b583c99fab2ce55397
puffynNeroun <130913559+puffynNeroun@users.noreply.github.com>
2024-10-24 20:27:57 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/illiacode/', // Убедитесь, что 'illiacode' заменено на название вашего репозитория
     build: {
-        outDir: 'dist',
+        outDir: 'dist', // Указывает, где будут сохраняться собранные файлы
         emptyOutDir: true,
     },
     server: {
-        open: true,
+        open: true, // Автоматически открывает браузер при запуске
         port: 5173,
     },
     resolve: {
         alias: {
-            '@': '/src',
+            '@': '/src', // Удобные пути к файлам внутри src
         },
     },
 });
